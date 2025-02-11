1cd0c79b2841e72096bccc5291067c320d5e1a73
readf0x <davis.a.forsythe@gmail.com>
2025-02-10 18:51:22 -0600

    init

---
+++ src/client/kotlin/org/foxtrot/neutron/CreateNeutronClient.kt
@@ -0,0 +1,9 @@
+package org.foxtrot.neutron
+
+import net.fabricmc.api.ClientModInitializer
+
+object CreateNeutronClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
